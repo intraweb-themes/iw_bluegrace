2344fc86f139bc62b2794d4ca257c52971a0f643
fcasanellas <fcasanellas@e968f4be-1634-4921-b96b-e3dd0a78059a>
2010-10-27 09:37:39 +0000

    new SVN structure

---
+++ version.php
@@ -0,0 +1,17 @@
+<?php
+$dom = ZLanguage::getThemeDomain('theme_iw_bluegrace'); 
+$themeversion['name'] = __('iw_bluegrace', $dom);
+$themeversion['url'] = __('iw_bluegrace', $dom);
+$themeversion['displayname'] = __('Intraweb BlueGrace', $dom);
+$themeversion['version'] = '1.0';
+$themeversion['description'] = __('Designed by Vladimir Prelovac and modified by Albert Bachiller. Xanthia theme by Charlie and modified by Toni Ginard.', $dom);
+$themeversion['author'] = 'Vladimir Prelovac, Charlie, Toni Ginard, Albert Bachiller';
+$themeversion['contact'] = 'http://phobos.xtec.cat/intraweb';
+$themeversion['admin'] = false;
+$themeversion['user'] = true;
+$themeversion['system'] = false;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'docs/<license file>';
+$themeversion['xhtml'] = true;
